57ea2718ffbc2c5ba8d1fd8282e9aff1b1c5c13d
Dominik <cogentdom@gmail.com>
2021-04-25 03:09:54 -0600

    ...

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,4 @@
+# %%
 import streamlit as st
 from datetime import date
 import yfinance as yf
@@ -8,8 +9,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import json
 
-
+# %%
 def main():
+    # %%
     style.use('seaborn-darkgrid')
     sns.set_context('notebook')
     sns.set_palette('gist_heat')
@@ -25,8 +27,8 @@ def main():
     # n_years = st.slider("Years of prediction:", 1, 3)
     period = 3 * 365
 
-
-    @st.cache
+# %%
+# @st.cache
     def load_data(ticker):
         df = yf.download(ticker, START, TODAY)
         df.reset_index(inplace=True)
@@ -44,16 +46,17 @@ def main():
     st.subheader('Raw data for one stock')
     st.write(data.tail())
 
-
+# %%
     def plot_raw_data(dataPrice):
         fig = go.Figure()
         fig.add_trace(go.Scatter(x=dataPrice['Date'], y=dataPrice['Open'], name='stock_open'))
         fig.add_trace(go.Scatter(x=dataPrice['Date'], y=dataPrice['Close'], name='stock_close'))
         fig.layout.update(title_text="Time Series Data (TSLA)", xaxis_rangeslider_visible=True)
         st.plotly_chart(fig)
-
-    # print(data)
-    plot_raw_data(data)
+# %%
+    print(stock_data)
+    # %%
+    plot_raw_data(stock_data['AAPL'])
 
     train = {}
 
@@ -75,22 +78,33 @@ def main():
         tmp['y'] = df['y'].tolist()
         train[k] = tmp
 
-
+# %%
     train_df = pd.DataFrame(train)
     train_df.to_json(r'./data.json')
     print('succesful write')
 
+# %%
     with open("./data.json", "r") as read_file:
         df2 = json.load(read_file)
     print('successful read')
 
+# %%
     for k in df2.keys():
         df3 = pd.DataFrame(df2[k])
         df3 = df3.set_index('ds')
         st.write(k)
         st.line_chart(df3)
 
+# %%
+    print(type(df2))
 
 
+
+
+
+
+# %%
 if __name__ == '__main__':
     main()
+
+# %%
